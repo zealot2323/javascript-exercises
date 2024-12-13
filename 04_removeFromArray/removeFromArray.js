2df11177618c9ff8393282b17fc81b24000b5a05
Eric Pratt <zealot2323@gmail.com>
2024-12-13 15:18:33 +0100

    Finished remaining exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 1; j <= arguments.length; j++) {
+            if(arguments[j] === array[i]) {
+                array.splice(i, 1);
+                i = 0;
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let low = 0;
+    let high = 0;
 
+    if(a < 0 || b < 0 || a % 1 != 0 || b % 1 != 0 || typeof a !== "number" || typeof b !== "number") {
+        return "ERROR"
+    }
+    else if(a > b) {
+        high = a;
+        low = b;
+    } else {
+        high = b;
+        low = a;
+    }
+    while(low <= high) {
+        sum = sum + low;
+        low++;
+    }
+    return sum
 };
 
 // Do not edit below this line
