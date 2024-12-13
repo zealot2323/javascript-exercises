a82a7971bdd51ad966795b18259251dd9bf81b61
Eric Pratt <zealot2323@gmail.com>
2024-12-13 13:43:54 +0100

    Completed first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repString = "";
+    if(num < 0) return repString = "ERROR";
+    for(let i=0; i < num; i++) {
+        repString += string;
+    }
+    return repString
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let returnString = "";
 
+    for(let i = string.length - 1; i >= 0; i--) {
+        returnString += string[i];
+    }
+    return returnString
 };
 
 // Do not edit below this line
